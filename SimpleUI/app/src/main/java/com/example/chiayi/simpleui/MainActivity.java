e2700cb9ff01fdf83fca68b34bd6e76d5fb2d6c8
joyshao37 <102306001@nccu.edu.tw>
2016-04-21 20:54:51 +0800

    implement text input and add radio button group

--- SimpleUI/app/src/main/java/com/example/chiayi/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/chiayi/simpleui/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.chiayi.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 //(Alt+Enter)import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //decide that the layout of MainActivity is activity_main.xml
         textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+
+        //interface
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+
+                    click(v);
+                    return true;
+
+                }
+            return false;
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
 
     }
 
     public void click(View view)
     {
-
-        textView.setText("Android Class");
+        String text = editText.getText().toString();
+        textView.setText(text);
+        editText.setText("");
     }
 
 
