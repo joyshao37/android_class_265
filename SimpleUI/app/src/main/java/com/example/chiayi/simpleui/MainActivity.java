54620185ba6d592fd1430eea13c3048073e33abe
joyshao37 <102306001@nccu.edu.tw>
2016-04-28 19:39:51 +0800

    listview set onitemclickListener

--- SimpleUI/app/src/main/java/com/example/chiayi/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/chiayi/simpleui/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.chiayi.simpleui;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 //(Alt+Enter)import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +25,12 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ArrayList<Order> orders;
     String drinkName = "black tea";
-
     String note = "";
     CheckBox checkBox;
+    ListView listview;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
         checkBox = (CheckBox)findViewById(R.id.hidecheckBox);
+        orders =new ArrayList<>();
+        listview = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
         //interface
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -61,14 +74,39 @@ public class MainActivity extends AppCompatActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton)findViewById(checkedId); //find any radio buttons
+                RadioButton radioButton = (RadioButton) findViewById(checkedId); //find any radio buttons
                 drinkName = radioButton.getText().toString();
                 //right-click then click Refactor-> can change the words in a time
             }
         });
 
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Order order= (Order) parent.getAdapter().getItem(position);
+                 // parent.getAdapter()->OrderAdapter();
+                 // getItem()的完全型態是Object->需Order轉型為order
+                //Toast.makeText(MainActivity.this, order.note, Toast.LENGTH_LONG).show();
+                Snackbar.make(view,order.note,Snackbar.LENGTH_LONG).show();
+            }
+        });
 
+    setupListView();
+    setupSpinner();
 
+    }
+
+    void setupListView()
+    {
+        OrderAdapter adapter = new OrderAdapter(this,orders);
+        listview.setAdapter(adapter);
+    }
+
+    void setupSpinner()
+    {
+        String [] data =getResources().getStringArray(R.array.StoreInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,data);
+        spinner.setAdapter(adapter);
     }
 
     public void click(View view)
@@ -76,7 +114,15 @@ public class MainActivity extends AppCompatActivity {
         note = editText.getText().toString();
         String text = note;
         textView.setText(text);
+
+        Order order = new Order();
+        order.drinkName = drinkName;
+        order.note=note;
+        order.storeInfo= (String)spinner.getSelectedItem();
+        orders.add(order);
+
         editText.setText("");
+        setupListView();
 
     }
 
---
+++ SimpleUI/app/src/main/java/com/example/chiayi/simpleui/Order.java
@@ -0,0 +1,10 @@
+package com.example.chiayi.simpleui;
+
+/**
+ * Created by chiayi on 2016/4/25.
+ */
+public class Order {
+    String note;
+    String drinkName;
+    String storeInfo;
+}
